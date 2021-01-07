13609162dbc283de8a25c9ed513f341706453b65
Ali Ozben <aliozben4@gmail.com>
2021-01-07 23:28:24 +0300

    Tooltip has been implemented.

---
+++ Assets/Scripts/Tooltip.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[ExecuteInEditMode()]
+public class Tooltip : MonoBehaviour {
+    public TextMeshProUGUI headerText;
+    public TextMeshProUGUI cooldownText;
+    public TextMeshProUGUI contentText;
+    public LayoutElement layoutElement;
+    public int characterWrapLimit;
+    public void SetText(string header, string cooldown, string content) {
+        headerText.text = "Spell Name: " + header;
+        cooldownText.text = "Cooldown: " + cooldown;
+        contentText.text = "\nDescription: " + content;
+        int cooldownLength = cooldownText.text.Length;
+        int contentLength = contentText.text.Length;
+        layoutElement.enabled = (cooldownLength > characterWrapLimit || contentLength > characterWrapLimit) ? true : false;
+    }
+    private void Update() {
+        Vector2 mousePos = Input.mousePosition;
+        transform.position = mousePos;
+    }
+}
---
+++ Assets/Scripts/TooltipSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipSystem : MonoBehaviour {
+    private static TooltipSystem instance;
+    public Tooltip tooltip;
+    public void Awake() {
+        instance = this;
+    }
+    public static void showTooltip(string header, string cooldown, string content) {
+        instance.tooltip.SetText(header, cooldown, content);
+        instance.tooltip.gameObject.SetActive(true);
+
+    }
+    public static void hideTooltip() {
+        instance.tooltip.gameObject.SetActive(false);
+    }
+}
